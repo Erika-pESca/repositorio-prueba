7df3eb5bf1c6099c2030bd43b03893e46bbb1a27
KennyMls11 <>
2025-11-14 21:14:34 -0500

    proyecto vacio, ignorar

--- src/wise-chat/dto/update-wise-chat.dto.ts
+++
@@ -1,4 +0,0 @@
-// import { PartialType } from '@nestjs/mapped-types';
-// import { CreateWiseChatDto } from './create-wisechat.dto';
-
-// export class UpdateWiseChatDto extends PartialType(CreateWiseChatDto) {}
